f9ac74b266e22488dbf13f3353aceade60abf4de
abhishekkb <abhishekkb@users.noreply.github.com>
2022-05-20 14:49:39 -0500

    Create FindShortestPath.java

---
+++ FindShortestPath/FindShortestPath.java
@@ -0,0 +1,48 @@
+public class FindShortestPath {
+    public static void main(String[] args) {
+        int[][]m = new int[4][4];
+        m[0][1] = 1;
+//        m[1][2] = 1;
+        m[2][2] = 1;
+        findNumPossiblePaths(m, 4, 4);
+        System.out.println("find number of paths " + count);
+//        Assetions.assertEquals(count, 4);
+    }
+
+
+
+    public static int count =0;
+    public static int findNumPossiblePaths(int[][] m, int mr, int mc) {
+        traverse(m, mc, mr, 0, 0);
+        return count;
+    }
+
+    public static void traverse(int[][] m, final int mc, final int mr, int x, int y) {
+        if(x == mc-1 && y == mr-1) {
+            count++;
+            return ;
+        }
+        if(canMoveRight(m, mc, x, y)) {
+            traverse(m, mc, mr, x+1, y);
+        }
+        if(canMoveDown(m, mr, x, y)) {
+            traverse(m, mc, mr, x, y+1);
+        }
+
+    }
+
+    public static boolean canMoveRight(int[][] m, int mc, int i, int j) {
+        if(i>=mc-1){
+            return false;
+        } else {
+            return !(m[i+1][j] == 1);
+        }
+    }
+    public static boolean canMoveDown(int[][] m, int mr, int i, int j) {
+        if(j>=mr-1){
+            return false;
+        } else {
+            return !(m[i][j+1] == 1);
+        }
+    }
+}
